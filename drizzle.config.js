5456af04c679c35ef47c8984924f208608c926e2
Code-Nethranand <nethranand12345@gmail.com>
2025-01-18 15:18:42 +0530

    Generating Interview Questions

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "drizzle-kit";
 
-
 export default defineConfig({
   schema: "./utils/schema.js",
   dialect: "postgresql",
--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
 import { neon } from '@neondatabase/serverless';
-import schema from './schema';
-import { drizzle } from 'drizzle-orm/neon-serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import * as schema from "./schema"
 
 const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_URL);
-const db = drizzle({ client: sql, schema });
+export const db = drizzle(sql,{schema});
